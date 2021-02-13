25e39f9342886f0cbe1ece28b18eb1bd3bea2ec2
blackdatgy <76581657+blackdatgy@users.noreply.github.com>
2021-02-13 11:16:33 +0530

    Initial Commit
    Initial Commit - Simple example of Functional programming

---
+++ src/com/java/programming/functional/GFG.java
@@ -0,0 +1,18 @@
+package com.java.programming.functional;
+
+public class GFG 
+{
+	public static void main(String[] args) 
+	{		
+		Runnable r = new Runnable() // Defining an anonymous method
+		{
+			public void run() 
+			{
+				System.out.println("Running in Runnable thread");
+			}
+		};
+
+		r.run();
+		System.out.println("Running in main thread");
+	}
+}
